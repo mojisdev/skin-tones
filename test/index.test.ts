92ccc609f984dc70229b3f4c9ac1a006bea6aa76
Lucas <lucasnrgaard@gmail.com>
2025-01-26 08:15:53 +0100

    test: enhance skin tone tests and add error handling for invalid tones

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,170 +1,124 @@
-import type { SkinTone } from "../src";
 import { describe, expect, it } from "vitest";
-import { getSkinTone, hasSkinTone, setMultipleSkinTones, setSkinTone } from "../src";
+import { getSkinTone, hasSkinTone, setSkinTone, setSkinTones, type SkinTone } from "../src";
 
 describe("set skin tone", () => {
-  it("should return the same emoji if skin tone is `none`", () => {
-    const emoji = "👍";
-    const expectedEmoji = emoji;
-    const result = setSkinTone(emoji, "none");
-    expect(result).toBe(expectedEmoji);
+  it("should throw error for invalid skin tone", () => {
+    expect(() => setSkinTone("👍", "invalid" as SkinTone)).toThrow("invalid skin tone: invalid");
   });
 
-  it("should add skin tone to emoji if skin tone is `light`", () => {
-    const emoji = "👍";
-    const expectedEmoji = "👍🏻";
-    const result = setSkinTone(emoji, "light");
-    expect(result).toBe(expectedEmoji);
+  it.each([
+    { emoji: "👍", tone: "none", expected: "👍" },
+    { emoji: "👍", tone: "light", expected: "👍🏻" },
+    { emoji: "👍", tone: "medium-light", expected: "👍🏼" },
+    { emoji: "👍", tone: "medium", expected: "👍🏽" },
+    { emoji: "👍", tone: "medium-dark", expected: "👍🏾" },
+    { emoji: "👍", tone: "dark", expected: "👍🏿" },
+    { emoji: "👍🏿", tone: "none", expected: "👍" },
+    { emoji: "👍🏿", tone: "light", expected: "👍🏻" },
+    { emoji: "👍🏿", tone: "medium-light", expected: "👍🏼" },
+    { emoji: "👍🏿", tone: "medium", expected: "👍🏽" },
+    { emoji: "👍🏿", tone: "medium-dark", expected: "👍🏾" },
+    { emoji: "👍🏿", tone: "dark", expected: "👍🏿" },
+
+    // without a skin tone
+    { emoji: "🌏", tone: "none", expected: "🌏" },
+    { emoji: "🌏", tone: "light", expected: "🌏" },
+    { emoji: "🌏", tone: "medium-light", expected: "🌏" },
+    { emoji: "🌏", tone: "medium", expected: "🌏" },
+
+    // tricky emojis
+    { emoji: "👸🏼", tone: "medium", expected: "👸🏽" },
+    { emoji: "🕵️‍♀️", tone: "medium-dark", expected: "🕵🏾‍♀" },
+    { emoji: "⛹️‍♀️", tone: "dark", expected: "⛹🏿‍♀" },
+    { emoji: "👩‍❤️‍👨", tone: "medium-dark", expected: "👩🏾‍❤‍👨🏾" },
+    { emoji: "👬", tone: "light", expected: "👬🏻" },
+    { emoji: "🤝", tone: "dark", expected: "🤝🏿" },
+  ] as const)("expect $emoji to be $expected", ({ emoji, tone, expected }) => {
+    const result = setSkinTone(emoji, tone);
+    expect(result).toBe(expected);
   });
 
-  it("should add skin tone to emoji if skin tone is `medium-light`", () => {
-    const emoji = "👍";
-    const expectedEmoji = "👍🏼";
-    const result = setSkinTone(emoji, "medium-light");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  it("should add skin tone to emoji if skin tone is `medium`", () => {
-    const emoji = "👍";
-    const expectedEmoji = "👍🏽";
-    const result = setSkinTone(emoji, "medium");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  it("should add skin tone to emoji if skin tone is `medium-dark`", () => {
-    const emoji = "👍";
-    const expectedEmoji = "👍🏾";
-    const result = setSkinTone(emoji, "medium-dark");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  it("should add skin tone to emoji if skin tone is `dark`", () => {
-    const emoji = "👍";
-    const expectedEmoji = "👍🏿";
-    const result = setSkinTone(emoji, "dark");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  it("should remove skin tone to emoji if skin tone is `none`", () => {
-    const emoji = "👍🏿";
-    const expectedEmoji = "👍";
-    const result = setSkinTone(emoji, "none");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  it("should add different skin tone to emoji", () => {
-    const emoji = "👸🏼";
-    const expectedEmoji = "👸🏽";
-    const result = setSkinTone(emoji, "medium");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  it("should return the same emoji if skin tone can't be set", () => {
-    const emoji = "🌍";
-    const expectedEmoji = "🌍";
-    const result = setSkinTone(emoji, "light");
-    expect(result).toBe(expectedEmoji);
-  });
-
-  describe("handle tricky emojis", () => {
-    it.each([
-      {
-        emoji: "🕵️‍♀️",
-        tone: "medium-dark",
-        expected: "🕵🏾‍♀",
-      },
-      {
-        emoji: "⛹️‍♀️",
-        tone: "dark",
-        expected: "⛹🏿‍♀",
-      },
-      {
-        emoji: "👩‍❤️‍👨",
-        tone: "medium-dark",
-        expected: "👩🏾‍❤‍👨🏾",
-      },
-      {
-        emoji: "👬",
-        tone: "light",
-        expected: "👬🏻",
-      },
-    ] as const)("expect $emoji to be $expected", ({ emoji, tone, expected }) => {
-      const result = setSkinTone(emoji, tone);
-      expect(result).toBe(expected);
-    });
-  });
-
-  describe("handle emojis without support for skin tones", () => {
-    it.each([
-      ["👩‍👦", "medium-dark", "👩‍👦"],
-      ["👩‍👩‍👧‍👧", "light", "👩‍👩‍👧‍👧"],
-    ] as const)("should ignore %s", (emoji, tone, expected) => {
-      const result = setSkinTone(emoji, tone);
-      expect(result).toBe(expected);
-    });
+  it.each([
+    ["👩‍👦", "medium-dark", "👩‍👦"],
+    ["👩‍👩‍👧‍👧", "light", "👩‍👩‍👧‍👧"],
+  ] as const)("should ignore %s", (emoji, tone, expected) => {
+    const result = setSkinTone(emoji, tone);
+    expect(result).toBe(expected);
   });
 });
 
-describe("get skin tone", () => {
-  it("should return `none` for emoji without skin tone", () => {
-    expect(getSkinTone("👍")).toBe("none");
+interface TestSetSkinTones {
+  emoji: string;
+  tones: SkinTone[];
+  expected: string;
+}
+
+describe("set skin tones", () => {
+  it("should throw error for invalid skin tones", () => {
+    expect(() => setSkinTones("👍", ["invalid" as SkinTone])).toThrow("invalid skin tone in: invalid");
   });
 
-  it("should return `light` for emoji with `light` skin tone", () => {
-    expect(getSkinTone("👍🏻")).toBe("light");
+  it.each([
+    { emoji: "👍", tones: ["none"], expected: "👍" },
+    { emoji: "👍", tones: ["light"], expected: "👍🏻" },
+    { emoji: "👍", tones: ["medium-light"], expected: "👍🏼" },
+    { emoji: "👍", tones: ["medium"], expected: "👍🏽" },
+    { emoji: "👍", tones: ["medium-dark"], expected: "👍🏾" },
+    { emoji: "👍", tones: ["dark"], expected: "👍🏿" },
+    { emoji: "👍🏿", tones: ["none"], expected: "👍" },
+    { emoji: "👍🏿", tones: ["light"], expected: "👍🏻" },
+    { emoji: "👍🏿", tones: ["medium-light"], expected: "👍🏼" },
+    { emoji: "👍🏿", tones: ["medium"], expected: "👍🏽" },
+    { emoji: "👍🏿", tones: ["medium-dark"], expected: "👍🏾" },
+    { emoji: "👍🏿", tones: ["dark"], expected: "👍🏿" },
+    { emoji: "👸🏼", tones: ["medium"], expected: "👸🏽" },
+    { emoji: "🕵️‍♀️", tones: ["medium-dark"], expected: "🕵🏾‍♀" },
+    { emoji: "⛹️‍♀️", tones: ["dark"], expected: "⛹🏿‍♀" },
+    { emoji: "👩‍❤️‍👨", tones: ["medium-dark"], expected: "👩🏾‍❤‍👨🏾" },
+    { emoji: "👬", tones: ["light"], expected: "👬🏻" },
+
+    // multiple tones
+    { emoji: "👩‍❤️‍👨", tones: ["light", "dark"], expected: "👩🏻‍❤‍👨🏿" },
+    { emoji: "🫱🏿‍🫲🏾", tones: ["medium-light", "medium"], expected: "🫱🏼‍🫲🏽" },
+    { emoji: "👨‍👩‍👧‍👦", tones: ["light", "dark"], expected: "👨‍👩‍👧‍👦" },
+  ] satisfies TestSetSkinTones[])("expect $emoji to be $expected", ({ emoji, tones, expected }) => {
+    expect(setSkinTones(emoji, tones)).toBe(expected);
   });
 
-  it("should return `medium-light` for emoji with `medium-light` skin tone", () => {
-    expect(getSkinTone("👍🏼")).toBe("medium-light");
+  it("should return unchanged emoji when no tones provided", () => {
+    expect(setSkinTones("👍", [])).toBe("👍");
   });
 
-  it("should return `medium` for emoji with `medium` skin tone", () => {
-    expect(getSkinTone("👍🏽")).toBe("medium");
-  });
-
-  it("should return `medium-dark` for emoji with `medium-dark` skin tone", () => {
-    expect(getSkinTone("👍🏾")).toBe("medium-dark");
-  });
-
-  it("should return `dark` for emoji with `dark` skin tone", () => {
-    expect(getSkinTone("👍🏿")).toBe("dark");
-  });
-
-  it("should return none for non-modifiable emoji", () => {
-    expect(getSkinTone("🌍")).toBe("none");
-  });
-
-  it("should return the first skin tone for emoji with multiple skin tones", () => {
-    expect(getSkinTone("👩🏼‍❤️‍👨🏿")).toBe("medium-light");
+  it("should return unchanged emoji when all tones are none", () => {
+    expect(setSkinTones("👍", ["none", "none"])).toBe("👍");
   });
 });
 
-describe("has skin tone", () => {
-  it("should return false for emoji without skin tone", () => {
-    expect(hasSkinTone("👍")).toBe(false);
-  });
+it.each([
+  { emoji: "👍", expected: "none" },
+  { emoji: "👍🏻", expected: "light" },
+  { emoji: "👍🏼", expected: "medium-light" },
+  { emoji: "👍🏽", expected: "medium" },
+  { emoji: "👍🏾", expected: "medium-dark" },
+  { emoji: "👍🏿", expected: "dark" },
+  { emoji: "🌍", expected: "none" },
 
-  it("should return true for emoji with white skin tone", () => {
-    expect(hasSkinTone("👍🏻")).toBe(true);
-  });
+  // multiple skin tones
+  { emoji: "👩🏼‍❤️‍👨🏿", expected: ["medium-light", "dark"] },
+  { emoji: "🫱🏿‍🫲🏾", expected: ["dark", "medium-dark"] },
+])("expect $emoji to have skin tone: $expected", ({ emoji, expected }) => {
+  expect(getSkinTone(emoji)).toStrictEqual(expected);
+});
 
-  it("should return true for emoji with cream skin tone", () => {
-    expect(hasSkinTone("👍🏼")).toBe(true);
-  });
-
-  it("should return true for emoji with light skin tone", () => {
-    expect(hasSkinTone("👍🏽")).toBe(true);
-  });
-
-  it("should return true for emoji with brown skin tone", () => {
-    expect(hasSkinTone("👍🏾")).toBe(true);
-  });
-
-  it("should return true for emoji with dark skin tone", () => {
-    expect(hasSkinTone("👍🏿")).toBe(true);
-  });
-
-  it("should return false for non-modifiable emoji", () => {
-    expect(hasSkinTone("🌍")).toBe(false);
-  });
+it.each([
+  ["👍", false],
+  ["👍🏻", true],
+  ["👍🏼", true],
+  ["👍🏽", true],
+  ["👍🏾", true],
+  ["👍🏿", true],
+  ["🌍", false],
+  ["👩🏻‍❤️‍👨🏿", true],
+])("expect %s to have skin tone: %s", (emoji, expected) => {
+  expect(hasSkinTone(emoji)).toBe(expected);
 });
